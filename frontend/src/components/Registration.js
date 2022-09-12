499add35874490112df86f0fb8f5c827ed466fb2
anoop ks <anoopsivan8@gmail.com>
2022-09-12 19:55:40 +0530

    registration page created

--- frontend/src/components/Registration.js
+++ frontend/src/components/Registration.js
@@ -3,12 +3,33 @@ import '../index.css';
 
 const Register = () => {
     return(
-        <div className="main-div">
+        <div className=" container main-div">
             <div className="heading-div">
-            <h1>Todo List</h1>
-
+                <h1>Todo List</h1>
             </div>
+                <div className="register-div">
+                    <div className="register-heading">
+                        <h2>Register</h2>
+                    </div>
+                    <div className="email-div">
+                        <input name ="text" type ="text" placeholder="Name" className="email-field"/>
+                    </div>
+                    <div className="email-div">
+                        <input name ="email" type ="text" placeholder="Email" className="email-field"/>
+                    </div>
+                    <div className="email-div">
+                        <input name ="email" type ="text" placeholder="Password" className="email-field"/>
+                    </div>
+                    <div className="password-div">
+                        <input name= "password" type ="password" placeholder="Confirm Password" className="password-field"/>
+                    </div>
+                    <div className="registerbutton-div">
+                        <button className="registerbutton" >Register</button>
+                    </div>
+                    
+                    
+                </div>
         </div>
     )
 }
-export default Register;
+export default Register;   
