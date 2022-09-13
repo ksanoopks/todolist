806a63914b97d0c88ab8d92e20ccd1a59179c98a
viswajithRoop <viswajitroop1@gmail.com>
2022-09-13 10:27:47 +0530

    Created a basic guest home page

---
+++ frontend/src/components/Guest.js
@@ -0,0 +1,54 @@
+import React from "react";
+import '../index.css'
+
+const Guest = () =>{
+    return(
+        <div className="main-div">
+            <div className="heading-guest">
+                <h1>Todo List</h1>
+            </div>
+            <div className="row">
+                <div className="col-2 sidebar">
+                    <div className="sidebar-content">
+                        <div className="home-div">
+                        <a className="home-link" href="/guest">Home</a>
+                        </div>
+                        <div className="logout-div">
+                        <a className="logout-link" href="/">Logout</a>
+                        </div>
+                    </div>
+                </div>
+                <div className="col content-bar">
+                    <div className="container table-div">
+                    <table className="table">
+                        <tr>
+                            <th>Username</th>
+                            <th>Date</th>
+                            <th>Todo List</th>
+                        </tr>
+                        <tr>
+                            <td>viswajith</td>
+                            <td>15/09/2022</td>
+                            <td>Complete chapter 23</td>
+                        </tr>
+                        <tr>
+                            <td>Anoop</td>
+                            <td>16/09/2022</td>
+                            <td>Buy apple</td>
+                        </tr>
+                        <tr>
+                            <td>Mathew</td>
+                            <td>16/09/2022</td>
+                            <td>Practise english</td>
+                        </tr>
+                    </table>
+                    </div>
+                </div>
+                
+            </div>
+
+        </div>
+    )
+}
+
+export default Guest;
