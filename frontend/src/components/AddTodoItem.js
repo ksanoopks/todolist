a2a6f64c357fbacfc8a1445d82bce34c6b09baf9
MathewRej <mathewrejic@gmail.com>
2022-09-15 11:05:28 +0530

    created a page for adding a todo item

---
+++ frontend/src/components/AddTodoItem.js
@@ -0,0 +1,73 @@
+import React, {useState} from 'react';
+import '../index.css';
+
+const AddTodoItem = () => {
+    const [values, setValues] = useState(
+        Object.assign({
+            name: '',
+            date: '',
+        })
+    )
+    const [errors, setErrors] = useState({})
+    const handleChange = (e) => {
+        const{name, value} = e.target
+        setValues({
+            ...values,
+            [name]:value,
+        })
+    }
+
+    const handleSubmit = () => {
+        let name = '';
+        let date = '';
+        const nameFormat = /^[A-Za-z]+/
+        if(values.name == ''){
+            name ='Add a Todo List'
+        }
+        if(values.name && !nameFormat.test(values.name)){
+            name= "Invalid Name"
+         }
+        if(values.date == ''){
+            date = 'Select a date'
+        }
+        setErrors({
+            name,
+            date,
+        })
+    }
+    
+    return(
+            <div className="modal-main">
+                        <div className="todolist-heading">
+                            <h2>Add a Todo Item</h2>
+                        </div>
+                        <div className="addtodo-div">
+                            <input className="addtodo-field" 
+                                   name= "name" 
+                                   type ="text" 
+                                   value = {values.name}
+                                   onChange = {(e) => {handleChange(e)}}
+                                   placeholder="Add a task"
+                                  />
+                        </div>
+                        <div className='errormessage-div'>
+                            {errors.name ? <label style={{color:'red'}}>{errors.name}</label> : null}
+                        </div>
+                        <div className='addtodo-div'>
+                            <input type = "date" 
+                                   name = "date" 
+                                   placeholder='Date' 
+                                   value = {values.date} 
+                                   onChange = {(e) => {handleChange(e)}} 
+                                   className='date-field'/>
+                        </div>
+                        <div className='errormessage-div'>
+                            {errors.date ? <label style={{color:'red'}}>{errors.date}</label> : null}
+                        </div>
+                        <div className="todobtn-div">
+                            <button className="addbutton-todolist" onClick={() => handleSubmit()}>Add</button>
+                        </div>
+                    </div>
+    )
+}
+export default AddTodoItem;
