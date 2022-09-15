9a6a47588c29c5d962890ce5bf35796d0ed00845
MathewRej <mathewrejic@gmail.com>
2022-09-15 11:10:50 +0530

    adding a todo list into user page adding todo list to database and viewing it on user home page

---
+++ frontend/src/components/Users.js
@@ -0,0 +1,86 @@
+import React, {useState, useEffect} from "react";
+import '../index.css';
+import AddIcon from '@mui/icons-material/Add';
+import Modal from 'react-modal';
+import AddTodoList from "./AddTodoList";
+import axios from "axios";
+import AddTodoItem from "./AddTodoItem";
+
+
+
+
+const customStyles = {
+    content: {
+      top: '50%',
+      left: '50%',
+      right: 'auto',
+      bottom: 'auto',
+      marginRight: '-50%',
+      transform: 'translate(-50%, -50%)',
+      backgroundColor: "rgb(159 163 163)",
+      height: 'auto',
+      width: '30%',
+      border: 'solid',
+      fontSize: '70%',
+    },
+  };
+
+const User = () => {
+   
+    const [data, setData] = useState([])
+    useEffect(() => {
+        axios({
+            method: 'get',
+            url: 'http://127.0.0.1:5000/addtodolist'
+        }).then(resp => {
+            console.log("data",resp.data)
+            setData(
+                resp.data
+            )
+        })
+      }, [])
+   
+    const[modalIsOpen, setModalIsOpen] = useState(false)
+    const closeModal = () => (
+        setModalIsOpen(false)
+    )
+    return(
+<div className="container-fluid main-div">
+    <div className="heading-div">
+        <h1>Todo List</h1>
+    </div>
+    <div className="row">
+        <div className="col-2 sidebar-div">
+            <div className="sidebarcontent-div">
+                <div><h3>Todo List</h3>
+                </div>
+                <div className="modal-body">
+                <Modal
+                isOpen={modalIsOpen}
+                ariaHideApp={false}
+                onRequestClose = {closeModal}
+                style={customStyles}>
+                    <AddTodoList/>
+                </Modal>
+                <button onClick ={ () => setModalIsOpen(true)} className="addicon-btn" ><AddIcon sx={{ fontSize: 40 }}/></button>
+                </div> 
+                </div>
+                <div>
+                <ul>
+                    {data.map((item, key) => {
+                        return(
+                                <li key = {key}  >{item.name}</li>
+                        )
+                    })}
+                    </ul>
+                </div> 
+        </div>
+        <div className="col content-div">
+
+        </div>
+    </div>
+
+</div>
+    )
+}
+export default User;
